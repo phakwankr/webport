59682a594d2779d37576f47e2e67112adc643ef7
Phakwan Kruerattanakul <114897102+phakwankr@users.noreply.github.com>
2023-02-11 07:38:54 +0000

    edited

---
+++ src/navbar.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+export default function CenteredTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Home" />
+        <Tab label="About" />
+        <Tab label="Portfolio" />
+        <Tab label="Experience" />
+      </Tabs>
+    </Box>
+  );
+}
